ee5ca47c28f4abe4fad1de691f81e0b61e4cbf79
Suhasini Padmanathan <suhasini_95@hotmail.com>
2024-05-31 11:17:59 -0400

    robots detected! research to circumvent blocking

--- scrapeWalmart.py
+++ scrapeWalmart.py
@@ -1,7 +1,7 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
-
+import requests
 """
 Categories of interes:
 
@@ -29,5 +29,16 @@ X fresh meat/seafood. only prepackaged
 
 """
 
+# html = urlopen("https://www.walmart.ca/en/browse/grocery/meat-seafood-alternatives/fresh-chicken-turkey/10019_6000194327357_6000194327409?icid=landing/cp_page_grocery_shop_all_chicken_17000_1MS64GM8BT")
+# bs = BeautifulSoup(html.read(), "html.parser")
 
-html = urlopen("")
+# print(bs)
+
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:39.0)'}
+url= "https://www.controlleremea.co.uk/"
+
+html_page = requests.get(url, headers=headers)
+
+bs  = BeautifulSoup (html_page.text, 'html.parser')
+
+print(bs)
