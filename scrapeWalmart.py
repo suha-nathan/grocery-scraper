97d0dbc65678cc1d8208bb3d24392e8c9288e281
Suhasini Padmanathan <suhasini_95@hotmail.com>
2024-06-03 13:45:49 -0400

    using selenium

--- scrapeWalmart.py
+++ scrapeWalmart.py
@@ -1,27 +1,29 @@
-from urllib.request import urlopen
+import os
+import time
+from selenium import webdriver
+from dotenv import load_dotenv
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
 from bs4 import BeautifulSoup
 import re
 import requests
 
+#load variables from .env file
+load_dotenv()
 
-import requests
-from bs4 import BeautifulSoup
-session = requests.Session()
-headers = {
-    'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5)'
-    'AppleWebKit 537.36 (KHTML, like Gecko) Chrome',
-    'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'}
-url = 'https://www.walmart.ca/en'
-req = session.get(url, headers=headers)
-bs = BeautifulSoup(req.text, 'html.parser')
-# print(bs.find('table', {'class':'table-striped'}).get_text)
+driver_path = os.getenv("DRIVER_PATH")
 
-"""
-Mobile user agent:
-User-Agent:Mozilla/5.0 (iPhone; CPU iPhone OS 7_1_2 like Mac OS X)
-AppleWebKit/537.51.2 (KHTML, like Gecko) Version/7.0 Mobile/11D257
-Safari/9537.53
+# Setup options to run headless
+chrome_options = Options()
+chrome_options.add_argument("--headless")
 
-Cookies:
-https://www.editthiscookie.com/
-"""
+service = Service(driver_path)
+
+driver = webdriver.Chrome(service=service, options=chrome_options) 
+
+driver.get("https://www.walmart.ca/en")
+time.sleep(3)
+
+print(driver.find_element(By.TAG_NAME, "button"))
